7ea30ec4e5883484afae2587cf611d07a0944f28
rhuam-barbosa <rhuambarbosa@icloud.com>
2026-04-13 16:19:39 +0200

    Initial commit to creating the application.

---
+++ src/main/java/com/bcnc/challenge/pricing/PricingEngineRetailApplication.java
@@ -0,0 +1,13 @@
+package com.bcnc.challenge.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingEngineRetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingEngineRetailApplication.class, args);
+    }
+
+}
